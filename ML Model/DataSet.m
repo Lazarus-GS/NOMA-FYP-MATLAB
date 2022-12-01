c5092ecb23b6f57e50dc8cdf2484bba6fe3910b4
Geethaka Shyamal Perera <geethakashyamal@gmail.com>
2022-12-01 11:21:04 +0530

    BER noma

--- ML Model/DataSet.m
+++ ML Model/DataSet.m
@@ -180,6 +180,60 @@ for u = 1:p
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
 %BER
+%NOMA
+
+%Do superposition coding
+    x = sqrt(pt(u))*(sqrt(a1)*x1 + sqrt(a2)*x2);
+    %Received signals
+    y1 = h1.*x + w1;
+    y2 = h2.*x + w2;
+    
+    %Equalize 
+    eq1 = y1./h1;
+    eq2 = y2./h2;
+    
+    %AT USER 1--------------------
+    %Direct decoding of x1 from y1
+    x1_hat = zeros(1,N);
+    x1_hat(eq1>0) = 1;
+    
+    %Compare decoded x1_hat with data1 to estimate BER
+    ber1(u) = biterr(data1,x1_hat)/N;
+    
+    %----------------------------------
+    
+    %AT USER 2-------------------------
+    %Direct decoding of x1 from y2
+    x12_hat = ones(1,N);
+    x12_hat(eq2<0) = -1;
+    
+    y2_dash = eq2 - sqrt(a1*pt(u))*x12_hat;
+    x2_hat = zeros(1,N);
+    x2_hat(real(y2_dash)>0) = 1;
+    
+    ber2(u) = biterr(x2_hat, data2)/N;
+    %-----------------------------------   
+    
+    gam_a = 2*((sqrt(a1*pt(u))-sqrt(a2*pt(u)))^2)*mean(g1)/no;
+    gam_b = 2*((sqrt(a1*pt(u))+sqrt(a2*pt(u)))^2)*mean(g1)/no;
+    ber_th1(u) = 0.25*(2 - sqrt(gam_a/(2+gam_a)) - sqrt(gam_b/(2+gam_b)));
+    
+    gam_c = 2*a2*pt(u)*mean(g2)/no;
+    gam_d = 2*((sqrt(a2) + sqrt(a1))^2)*pt(u)*mean(g2)/no;
+    gam_e = 2*((sqrt(a2) + 2*sqrt(a1))^2)*pt(u)*mean(g2)/no;
+    gam_f = 2*((-sqrt(a2) + sqrt(a1))^2)*pt(u)*mean(g2)/no;
+    gam_g = 2*((-sqrt(a2) + 2*sqrt(a1))^2)*pt(u)*mean(g2)/no;
+    
+    gc = (1 - sqrt(gam_c/(2+gam_c)));
+    gd = (1-sqrt(gam_d/(2+gam_d)));
+    ge = (1-sqrt(gam_e/(2+gam_e)));
+    gf = (1-sqrt(gam_f/(2+gam_f)));
+    gg = (1-sqrt(gam_g/(2+gam_g)));
+    
+    ber_th2(u) = 0.5*gc - 0.25*gd + 0.25*(ge+gf-gg);
+    
+    gamma1(u) = a1*pt(u)*mean(g1)/(a2*pt(u)*mean(g1) + no);
+    gamma2(u) = a2*pt(u)*mean(g2)/no;
 
     
 end
