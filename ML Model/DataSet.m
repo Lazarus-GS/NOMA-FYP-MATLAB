50a57d8d73f8bc7ba9fa133ea876d9a661122351
Geethaka Shyamal Perera <geethakashyamal@gmail.com>
2022-12-03 00:00:55 +0530

    Update DataSet.m

--- ML Model/DataSet.m
+++ ML Model/DataSet.m
@@ -11,18 +11,6 @@ rate1 = 1; rate2 = 2;       %Target rate of users in bps/Hz
 N = 10^4;
 dgap = [0.1,1,100]; %in meters
 
-%Generate noise samples for both users
-w1 = sqrt(no)*(randn(1,N)+1i*randn(1,N))/sqrt(2);
-w2 = sqrt(no)*(randn(1,N)+1i*randn(1,N))/sqrt(2);
-
-%Generate random binary data for two users
-data1 = randi([0 1],1,N);  %Data bits of user 1
-data2 = randi([0 1],1,N);  %Data bits of user 2
-
-%Do BPSK modulation of data
-x1 = 2*data1 - 1;
-x2 = 2*data2 - 1;
-
 
 for i = 1:3 %dgap values loop 
 d2 = 2;      %near user %Distance of users
@@ -42,6 +30,18 @@ No = -174 + 10*log10(BW);
 no = (10^-3)*db2pow(No);
 
 SNR = Pt - No;
+
+%Generate noise samples for both users
+w1 = sqrt(no)*(randn(1,N)+1i*randn(1,N))/sqrt(2);
+w2 = sqrt(no)*(randn(1,N)+1i*randn(1,N))/sqrt(2);
+
+%Generate random binary data for two users
+data1 = randi([0 1],1,N);  %Data bits of user 1
+data2 = randi([0 1],1,N);  %Data bits of user 2
+
+%Do BPSK modulation of data
+x1 = 2*data1 - 1;
+x2 = 2*data2 - 1;
 
 %Pt = 0:2:40;                %Transmit power in dBm
 %pt = (10^-3)*10.^(Pt/10);   %Transmit power in linear scale
@@ -182,7 +182,7 @@ for u = 1:p
 %BER
 %NOMA
 
-%Do superposition coding
+    %Do superposition coding
     x = sqrt(pt(u))*(sqrt(a1)*x1 + sqrt(a2)*x2);
     %Received signals
     y1 = h1.*x + w1;
@@ -212,30 +212,12 @@ for u = 1:p
     x2_hat(real(y2_dash)>0) = 1;
     
     ber2(u) = biterr(x2_hat, data2)/N;
-    %-----------------------------------   
-    
-    gam_a = 2*((sqrt(a1*pt(u))-sqrt(a2*pt(u)))^2)*mean(g1)/no;
-    gam_b = 2*((sqrt(a1*pt(u))+sqrt(a2*pt(u)))^2)*mean(g1)/no;
-    ber_th1(u) = 0.25*(2 - sqrt(gam_a/(2+gam_a)) - sqrt(gam_b/(2+gam_b)));
-    
-    gam_c = 2*a2*pt(u)*mean(g2)/no;
-    gam_d = 2*((sqrt(a2) + sqrt(a1))^2)*pt(u)*mean(g2)/no;
-    gam_e = 2*((sqrt(a2) + 2*sqrt(a1))^2)*pt(u)*mean(g2)/no;
-    gam_f = 2*((-sqrt(a2) + sqrt(a1))^2)*pt(u)*mean(g2)/no;
-    gam_g = 2*((-sqrt(a2) + 2*sqrt(a1))^2)*pt(u)*mean(g2)/no;
-    
-    gc = (1 - sqrt(gam_c/(2+gam_c)));
-    gd = (1-sqrt(gam_d/(2+gam_d)));
-    ge = (1-sqrt(gam_e/(2+gam_e)));
-    gf = (1-sqrt(gam_f/(2+gam_f)));
-    gg = (1-sqrt(gam_g/(2+gam_g)));
-    
-    ber_th2(u) = 0.5*gc - 0.25*gd + 0.25*(ge+gf-gg);
-    
-    gamma1(u) = a1*pt(u)*mean(g1)/(a2*pt(u)*mean(g1) + no);
-    gamma2(u) = a2*pt(u)*mean(g2)/no;
+    %-----------------------------------
 
     
+%OMA
+
+
 end
 
 
